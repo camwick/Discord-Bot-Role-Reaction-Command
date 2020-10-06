d81b00bcac87622711a4cb4adbd1142a02f50127
camwick <54330525+camwick@users.noreply.github.com>
2020-10-05 22:37:33 -0500

    Update main.js
    Added more comments and got rid of some redundent code

--- main.js
+++ main.js
@@ -1,13 +1,18 @@
 const Discord = require('discord.js');
 const client = new Discord.Client({ partials: ["MESSAGE", 'CHANNEL', 'REACTION']});
+
+// saved my bot key in a json file on my pc so that I could upload share this file
 const TOKEN = require('./config.json')
 
+// prefix to distinguish commands
 const PREFIX = '!';
 
+// ensures bot got online by logging it in console
 client.on('ready', () => {
     console.log('Bot is online');
 })
 
+// handles commands
 client.on('message', async message => {
     if (!message.content.startsWith(PREFIX)) return;
     
@@ -15,9 +20,11 @@ client.on('message', async message => {
     const args = message.content.slice(PREFIX.length).split(/ +/);
     const command = args.shift().toLowerCase();
 
+    // roles command
     // sends an Embeded message that allows for role assignment via reactions
     if (command === 'roles')
     {
+        // creating embedded message
         let embed = new Discord.MessageEmbed()
         .setTitle('Reaction Roles')
         .setDescription(':robot_face: - Discord Bot\n' + 
@@ -25,6 +32,8 @@ client.on('message', async message => {
         ':video_game: - Game')
         .setColor('BLUE')
         let msgEmbed = await message.channel.send(embed)
+        
+        // adding the reactions to the embed
         msgEmbed.react('🤖')
         msgEmbed.react('🖥️')
         msgEmbed.react('🎮')
@@ -36,9 +45,11 @@ client.on('messageReactionAdd', async (reaction, user) => {
     if (reaction.message.partial) await reaction.message.fetch();
     if (reaction.partial) await reaction.fetch;
 
+    // making sure reaction isn't by the bot
     if(user.bot) return;
-    if(!reaction.message.guild) return;
+   
 
+    // assigning role based off reaction choice
     if (reaction.emoji.name === '🤖')
     {
         await reaction.message.guild.members.cache.get(user.id).roles.add('762734541344342056')
@@ -59,9 +70,10 @@ client.on('messageReactionRemove', async (reaction, user) => {
     if (reaction.message.partial) await reaction.message.fetch();
     if (reaction.partial) await reaction.fetch;
 
+    // making sure reaction isn't by the bot
     if(user.bot) return;
-    if(!reaction.message.guild) return;
 
+    // removing role when reaction is removed
     if (reaction.emoji.name === '🤖')
     {
         await reaction.message.guild.members.cache.get(user.id).roles.remove('762734541344342056')
